fe78831f479f6f465013e1bc9539b5f4df47b5a1
Alex Oladele <dragid10@gmail.com>
2017-01-09 18:20:31 -0500

    Initial Commit

--- form.js
+++ form.js
@@ -1,10 +1,10 @@
 $(function () { // Waits until document has loaded before it proceeds with any of the JS
 
-    // Hides The left column when the hide button is clicked
-    $("#HideLeft").click(function () {
-        $("#Left").fadeOut();
-        console.log("Button clicked, should hide");
-    });
+    // var formData = JSON.parse($.get("http://campbest.270e.csi.miamioh.edu/forms/form-ajax.php"));
+    $.get("http://campbest.270e.csi.miamioh.edu/forms/form-ajax.php", function (data, status) {
+        console.log("Status is: " + status + "\nData is: " + data);
+    }, "json");
+    console.log(formData);
 
     // Any of the selectors listed below will hide their respective alert boxes if the content in their input is more than 2 chars long
     $("#firstname, #lastname, #miamiuid, #hometown, #currentcity, #comment").keyup(function () {
